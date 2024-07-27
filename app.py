046e5e3215b5fc137269b872406199d2cb8a397b
Vignesh Ramesh <vigneshr@aarohaa.ai>
2024-07-26 17:19:15 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def news_webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return Response(status=200)
+
+app.run(host='0.0.0.0', port=8000)
+
+
+if __name__ == '__main__':
+    app.run()
