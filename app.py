5495483bb6ddac264145b649888260dc1c698afb
Vignesh Ramesh <vigneshr@aarohaa.ai>
2024-07-26 18:08:00 -0700

    Change webhook response type

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/webhook', methods=['POST'])
 def news_webhook():
     if request.method == 'POST':
         print("Data received from Finnhub Webhook is: ", request.json)
-        return Response(status=200)
+        return jsonify({'message': 'success'}), 200
 
 @app.route("/")
 def hello():
-  return 'News ingestion Webhook'
+    return 'News ingestion Webhook'
+
 
 # app.run(host='0.0.0.0', port=8000)
 
