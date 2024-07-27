9420f219e12bb4242fc763bd461d201887e80b3f
Vignesh Ramesh <vigneshr@aarohaa.ai>
2024-07-26 19:59:28 -0700

    Fix Response

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST', 'GET'])
 def news_webhook():
     print("Data received from Finnhub Webhook is: ", request.json)
-    return jsonify({'message': 'success'}), 200
-
+    if request.method == 'POST':
+        print("post")
+        return Response('post',status=200)
+    else:
+        print("get")
+        return Response('get',status=200)
 
 @app.route("/")
 def hello():
